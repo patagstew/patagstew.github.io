04ec78b717e58b200f3448ff2576569d277a5c70
patagstew <patagstew@gmail.com>
2025-12-11 11:19:34 -0500

    Delete Javascript directory

--- Javascript/gallery.js
+++
@@ -1,9 +0,0 @@
-const checkbox = document.querySelectorAll("#age");
-
-
-checkbox[0].onclick = function(){
-    alert("18+ was clicked!");
-}
-checkbox[1].onclick = function(){
-    alert("Less than 18 was clicked! You are still welcome to see the photos");
-}
